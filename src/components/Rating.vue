20e4b911bb9cfc1e7c5e51128e3d8935e0257f8f
Andre Tan <tanneman1@gmail.com>
2018-03-06 21:01:16 -0500

    In rating only show the amount of stars of the rating

--- src/components/Rating.vue
+++ src/components/Rating.vue
@@ -1,20 +1,11 @@
 <template>
   <div>
     <MdIcon v-for='idx in rating' :key="idx">star</MdIcon>
-    <MdIcon v-for='idx in ratingLeft' :key='idx + rating'>star_border</MdIcon>
   </div>
 </template>
 
 <script>
   export default {
-    props: ['rating'],
-    computed: {
-      ratingLeft () {
-        return this.rating > 5 ? 0 : 5 - this.rating
-      }
-    }
+    props: ['rating']
   }
 </script>
-
-<style scoped>
-</style>
